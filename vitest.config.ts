bfc9168f826c4a55fc8016c4504e4bf3bfef4fd2
nurodev <me@nuro.dev>
2023-04-01 14:43:47 +0100

    Added React plugin to Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,10 @@
 
 import { defineConfig } from "vitest/config";
 import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "~": resolve(__dirname, "src"),
