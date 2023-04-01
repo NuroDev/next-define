d0ee8f4025a40a18debeb63d86aae600663ad8ab
nurodev <me@nuro.dev>
2023-04-01 15:18:28 +0100

    Fixed `TProps` type by switching from `unknown` to `any`

--- src/page/page.action.tsx
+++ src/page/page.action.tsx
@@ -12,7 +12,7 @@ import type { Awaitable } from "~/shared";
 // Note: We custom alias here to update the return type
 // to allow it to optionally return a promise.
 type GetServerSideProps<
-  TProps extends Record<string, unknown>,
+  TProps extends Record<string, any>,
   TQuery extends ParsedUrlQuery = ParsedUrlQuery,
   TData extends PreviewData = PreviewData
 > = (
@@ -20,13 +20,13 @@ type GetServerSideProps<
 ) => Awaitable<GetServerSidePropsResult<TProps>>;
 
 interface PageOptions<
-  TProps extends Record<string, unknown> = Record<string, unknown>
+  TProps extends Record<string, any> = Record<string, any>
 > {
   Component: FC<TProps>;
 }
 
 interface SSRPageOptions<
-  TProps extends Record<string, unknown> = Record<string, unknown>,
+  TProps extends Record<string, any> = Record<string, any>,
   TQuery extends ParsedUrlQuery = ParsedUrlQuery,
   TData extends PreviewData = PreviewData,
   TSSRHandler extends GetServerSideProps<
@@ -43,7 +43,7 @@ interface SSRPageOptions<
 }
 
 interface StaticPropsPageOptions<
-  TProps extends Record<string, unknown> = Record<string, unknown>,
+  TProps extends Record<string, any> = Record<string, any>,
   TParams extends ParsedUrlQuery = ParsedUrlQuery,
   TData extends PreviewData = PreviewData,
   TGSPHandler extends GetStaticProps<TProps, TParams, TData> = GetStaticProps<
@@ -60,7 +60,7 @@ interface StaticPropsPageOptions<
 }
 
 export function page<
-  TProps extends Record<string, unknown> = Record<string, unknown>,
+  TProps extends Record<string, any> = Record<string, any>,
   TQuery extends ParsedUrlQuery = ParsedUrlQuery,
   TData extends PreviewData = PreviewData,
   TReturnProps = SSRPageOptions<TProps, TQuery, TData>["Component"],
@@ -68,7 +68,7 @@ export function page<
 >(options: SSRPageOptions<TProps, TQuery, TData>): typeof options;
 
 export function page<
-  TProps extends Record<string, unknown> = Record<string, unknown>,
+  TProps extends Record<string, any> = Record<string, any>,
   TParams extends ParsedUrlQuery = ParsedUrlQuery,
   TData extends PreviewData = PreviewData,
   TComponent extends () => JSX.Element = () => JSX.Element
@@ -79,7 +79,7 @@ export function page<
 // >(options: { Component: TComponent }): typeof options;
 
 export function page<
-  TProps extends Record<string, unknown> = Record<string, unknown>,
+  TProps extends Record<string, any> = Record<string, any>,
   TComponent extends FC<TProps> = FC<TProps>
 >(options: PageOptions<TProps>): typeof options;
 
