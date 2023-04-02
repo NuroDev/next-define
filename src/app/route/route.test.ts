ca3a6d1804a65c004d64a6a2aab707662a9766d1
nurodev <me@nuro.dev>
2023-04-02 19:35:08 +0100

    Added basic tests to app directory route module

--- src/app/route/route.test.ts
+++ src/app/route/route.test.ts
@@ -1,8 +1,50 @@
-import { describe, it, expect } from "vitest";
-import { number } from "minifaker";
+import { describe, it, expect, expectTypeOf } from "vitest";
+import { NextResponse } from "next/server";
 
-import { routeHandler } from ".";
+import { route } from ".";
 
-describe("routeHandler", () => {
-  it("true", () => expect(true).toEqual(true));
+import type { NextApiConfig } from "~/shared";
+import type { NextRouteHandler } from "./route.action";
+
+describe("route", () => {
+  it("Node.js - A basic GET handler", () => {
+    const { GET } = route({
+      GET: (_req, _ctx) => NextResponse.json({ message: "Hello World" }),
+    });
+
+    expect(GET).toBeDefined();
+    expect(GET).toBeInstanceOf(Function);
+    expectTypeOf(GET).toMatchTypeOf<NextRouteHandler>();
+  });
+
+  it("Node.js - A basic GET handlerwith typed parameters", () => {
+    interface Params {
+      message: string;
+    }
+
+    const { GET } = route<Params>({
+      GET: (_req, _ctx) => NextResponse.json({ message: "Hello World" }),
+    });
+
+    expect(GET).toBeDefined();
+    expect(GET).toBeInstanceOf(Function);
+    expectTypeOf(GET).toMatchTypeOf<
+      NextRouteHandler<Params, Response> | undefined
+    >();
+  });
+
+  it("Edge - A basic GET handler using the `edge` runtime", () => {
+    const { GET, runtime } = route({
+      runtime: "edge",
+      GET: (_req, _ctx) => NextResponse.json({ message: "Hello World" }),
+    });
+
+    expect(GET).toBeDefined();
+    expect(GET).toBeInstanceOf(Function);
+    expectTypeOf(GET).toMatchTypeOf<NextRouteHandler>();
+
+    expect(runtime).toBeDefined();
+    expect(runtime).toBe("edge");
+    expectTypeOf(runtime).toMatchTypeOf<NextApiConfig["runtime"]>();
+  });
 });
